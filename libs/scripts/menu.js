e209afdabb4031a975fd72c43b7e999217cf53b3
Simon <sima.chetverukhin@inn.ru>
2017-01-17 18:19:30 +0300

    we go on

--- libs/scripts/menu.js
+++ libs/scripts/menu.js
@@ -1,12 +1,25 @@
 var currentGame = 'pb';
 
+function resetClasses() {
+	$('.menu__item').removeClass('menu__item--active');
+	$('.menu__line').removeClass('menu__line--pb menu__line--bns menu__line--l2');
+	$('.illustration__game').removeClass('illustration__game--active');
+	$('.prizes__list').removeClass('prizes__list--active');
+	$('body').removeClass('pb l2 bns');
+	$('.welcome .right').removeClass('right--pb right--l2 right--bns');
+}
+
 function goGame(newGame) {
-
 	if (newGame !== currentGame) {
-		$('.menu__item').removeClass('.menu__item--active');
-		$('.menu__item')
+		resetClasses();
+		$('.menu__item--' + newGame).addClass('menu__item--active');
+		$('.menu__line').addClass('menu__line--' + newGame);
+		$('body').addClass(newGame);
+		$('.illustration__game--' + newGame).addClass('illustration__game--active');
+		$('.welcome .right').addClass('right--' + newGame);
+		$('.prizes__list--' + newGame).addClass('prizes__list--active');
+		currentGame = newGame;
 	}
-
 }
 
 $(function() {
@@ -14,6 +27,7 @@ $(function() {
 	$('.menu__item').click(function () {
 		var newGame = $(this).attr("data-game");
 		goGame(newGame)
-	})
+	});
 
+	return $(".illustration").stick_in_parent();
 });
